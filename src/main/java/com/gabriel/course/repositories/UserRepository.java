2ee2338ceadd3eb0ebe962298503fc0f53066dff
Gabriel Silva <gabiles278@gmail.com>
2023-07-28 12:54:12 -0300

    config h2 database

--- src/main/java/com/gabriel/course/repositories/UserRepository.java
+++ src/main/java/com/gabriel/course/repositories/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.gabriel.course.entities.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer>{
+public interface UserRepository extends JpaRepository<User, Long>{
 
 }
