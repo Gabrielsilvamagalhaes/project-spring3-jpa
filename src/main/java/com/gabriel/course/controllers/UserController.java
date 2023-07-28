43089311eef339312d855f95ba9b0311aafb2e19
Gabriel Silva <gabiles278@gmail.com>
2023-07-28 13:16:58 -0300

    add post method

--- src/main/java/com/gabriel/course/controllers/UserController.java
+++ src/main/java/com/gabriel/course/controllers/UserController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,11 @@ public class UserController {
 	public List<User> searchAll() {
 		return userRepository.findAll();
 		
+	}
+	
+	@PostMapping
+	public User save(@RequestBody User user) {
+		return userRepository.save(user);
 	}
 	
 }
