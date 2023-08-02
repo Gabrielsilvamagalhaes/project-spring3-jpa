965ea594034860cfc30a9a7a59ccb3119ca0f57c
Gabriel Silva <gabiles278@gmail.com>
2023-08-01 21:24:15 -0300

    refactoring userController

--- src/main/java/com/gabriel/course/controllers/UserController.java
+++ src/main/java/com/gabriel/course/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.gabriel.course.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gabriel.course.entities.User;
 import com.gabriel.course.services.UserService;
@@ -35,9 +37,14 @@ public class UserController {
 	}
 	
 	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
 	public ResponseEntity<User> save(@RequestBody User user) {
-		return ResponseEntity.ok().body(userService.save(user));
+		User temporaryUser = userService.save(user);
+		
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(temporaryUser.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(temporaryUser);
 	}
 	
 	@DeleteMapping("/{id}")
